c14c66acbc0bce21f2c2c5fb363cea097bc642a8
mbarreirinho <mbarreirinho@gmail.com>
2025-12-02 16:59:17 +0000

    Refactoring Providers

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,35 @@
+terraform {
+  required_version = ">= 1.0.7"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
+
+    vault = {
+      version = "3.0.1"
+    }
+  }
+}
+
+provider "vault" {
+  address = "http://localhost:8201"
+  token   = "f23612cf-824d-4206-9e94-e31a6dc8ee8d"
+}
+
+provider "vault" {
+  alias   = "vault_dev"
+  address = "http://localhost:8201"
+  token   = "f23612cf-824d-4206-9e94-e31a6dc8ee8d"
+}
+
+provider "vault" {
+  alias   = "vault_prod"
+  address = "http://localhost:8301"
+  token   = "083672fc-4471-4ec4-9b59-a285e463a973"
+}
+
 resource "vault_audit" "audit_dev" {
   provider = vault.vault_dev
   type     = "file"
